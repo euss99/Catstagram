6f8ecfb1a44c1ae451eb0366ac308ccf75409894
euss99 <euss99@hotmail.com>
2023-01-18 01:06:31 -0600

    Creando la estrucutura básica de los componentes del proyecto

---
+++ src/components/Main.svelte
@@ -0,0 +1,5 @@
+<div class="Main">
+    <div class="Main-container">
+        <slot /> <!-- slot me permitirá poder insertar los elementos que tenemos como hijos del componente -->
+    </div>
+</div>
