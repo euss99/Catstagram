2935fb6af9879a77988248fb5ad4e290a7e3f622
euss99 <euss99@hotmail.com>
2023-01-18 12:55:34 -0600

    Agregando los componentes de Header y Sidebar

---
+++ src/components/Profile.svelte
@@ -0,0 +1,12 @@
+<div class="Profile">
+    <div class="Profile-content">
+        <div class="Profile-avatar">
+            <img src="https://i.imgur.com/DrClYyD.jpg" alt="Imágen de perfil" />
+        </div>
+
+        <div class="Profile-info">
+            <h2>euss99</h2>
+            <span>Uriel Solís Salinas</span>
+        </div>
+    </div>
+</div>
